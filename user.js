a7903eaa6250e1e2926dadf1652a7453e480cf7f
someonewithnousername <39712430+someonewithnousername@users.noreply.github.com>
2018-05-29 21:22:23 +0200

    Added regex fixing the fjord and old galaries
    With the regex, the script now supports galleries with a number lower than 1000000 also changes the windows according to the site you're on.

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torrents done quick
 // @namespace    https://forums.e-hentai.org/index.php?showuser=2725435
-// @version      0.1
+// @version      0.2
 // @description  Embeding the torrent window in the original gallary
 // @author       someguywith
 // @match        https://e-hentai.org/g/*
@@ -10,15 +10,17 @@
 /*
 TO-DO
   Make the objects fit better
-  Ad an 'e-' > 'ex' conferter if current link is 'ex'
   Make both objects the same size, or atleast make the border look the same size.
 */
 var link = window.location.href;
-var gid = link.substr(23, 7); //dirty, I don't know how to search between characters
-var gt = link.substr(31, 10);
+var regex = new RegExp("https:\/\/e(?<id>.)hentai.org\/g\/(?<gid>.*)\/(?<gt>.*?)\/");
+var matches = regex.exec(window.location.href);
+var gid = matches.groups.gid;
+var gt = matches.groups.gt;
+var site = matches.groups.id;
 var torrent = document.createElement("div");
-var torrentobj = '<object type="text/html" data="https://e-hentai.org/gallerytorrents.php?gid=' + gid + '&t=' + gt + '" width="675" height="582"></object>';
-var expurgeobj = '<object type="text/html" data="https://e-hentai.org/gallerypopups.php?gid=' + gid + '&t=' + gt + '&act=expunge" width="675" height="582"></object>';
+var torrentobj = '<object type="text/html" data="https://e' +site+ 'hentai.org/gallerytorrents.php?gid=' + gid + '&t=' + gt + '" width="675" height="582"></object>';
+var expurgeobj = '<object type="text/html" data="https://e' +site+ 'hentai.org/gallerypopups.php?gid=' + gid + '&t=' + gt + '&act=expunge" width="675" height="582"></object>';
 
 torrent.innerHTML = '<div style="width:100%; overflow:hidden;"> ' + torrentobj + expurgeobj + '</div>';
 
